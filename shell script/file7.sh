118776716b7a292f30d7bef082dcecc380653004
sachin569 <79416001+sachin569@users.noreply.github.com>
2021-08-04 18:46:17 +0530

    Create file7.sh

---
+++ shell script/file7.sh
@@ -0,0 +1,18 @@
+#/!bin/bash
+filename="file1.txt"
+echo Start
+while read p; do
+        echo $p
+done 
+
+echo "enter n values as range to calculate even 
+numbers"
+read n
+i=1
+while [ $i - 1e $n ]
+do
+if [ ' expr  $i  %  2' -eq  0 ]
+then
+echo even = $i
+
+done
